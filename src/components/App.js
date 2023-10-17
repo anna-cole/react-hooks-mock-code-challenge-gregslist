f9beb9ba0d1c308f32e09b4eeda8f1013e9d8e51
Anna Raptidou Cole <acrrj123@gmail.com>
2023-10-16 21:49:50 -0500

    render listing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(resp => resp.json())
+    .then(items => setItems(items))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer items={items}/>
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,23 +1,24 @@
 import React from "react";
 
-function ListingCard() {
+function ListingCard({ item }) {
+  
   return (
-    <li className="card">
-      <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
-      </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
-      </div>
-    </li>
+  <li className="card">
+    <div className="image">
+      <span className="price">$0</span>
+      <img src={item.image} alt={item.description} />
+    </div>
+    <div className="details">
+      {true ? (
+        <button className="emoji-button favorite active">★</button>
+      ) : (
+        <button className="emoji-button favorite">☆</button>
+      )}
+      <strong>{item.description}</strong>
+      <span> · {item.location}</span>
+      <button className="emoji-button delete">🗑</button>
+    </div>
+  </li>
   );
 }
 
